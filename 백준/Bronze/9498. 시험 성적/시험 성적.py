b9bfecae84d5da24889c1fc01bce18f8477d10a5
MinGyu <114341409+Minn97u@users.noreply.github.com>
2024-04-19 17:43:39 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+c = int(input())
+
+if ( c >= 90 and c <= 100):
+    print("A")
+elif ( c >= 80 ):
+    print("B")
+elif ( c >= 70 ):
+    print("C")
+elif ( c >= 60 ):
+    print("D")
+else :
+    print("F")
