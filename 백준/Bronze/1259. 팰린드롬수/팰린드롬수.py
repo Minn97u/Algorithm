541f8e67dc767e26ec0a35eac2fce1e832989064
MinGyu <114341409+Minn97u@users.noreply.github.com>
2024-04-27 22:37:06 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,13 @@
+
+while(1):
+    a = input()
+    b = a[::-1]
+    if(a == '0'):
+        break;
+    elif (a == b):
+        print("yes")
+        continue
+    else:
+        print("no")
+        continue
+    
