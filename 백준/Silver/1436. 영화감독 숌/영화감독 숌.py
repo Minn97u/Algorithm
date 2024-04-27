fa3cf64c270a017ef25b1c5cd26dd1e3c082e2fe
MinGyu <114341409+Minn97u@users.noreply.github.com>
2024-04-27 22:49:08 +0900

    [Silver V] Title: 영화감독 숌, Time: 644 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,14 @@
+n = int(input())
+prove = 0
+result = 666
+
+while (1):
+    if '666' in str(result):
+        prove += 1
+        
+    if prove == n:
+        break
+    
+    result += 1
+    
+print(result)
