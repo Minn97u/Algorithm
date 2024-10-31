c8a0b789ea97c1efbf786b9e57f7fb364fd1e784
MinGyu <114341409+Minn97u@users.noreply.github.com>
2024-10-31 21:45:42 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 50924 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,10 +1,12 @@
 from collections import deque
+def find_card(N):
+    queue = deque(range(1, N+1))
+
+    while len(queue) > 1:
+        queue.popleft()
+        queue.append(queue.popleft())
+
+    return queue[0]
 
 N = int(input())
-queue = deque(range(1, N+1))
-
-while len(queue) > 1:
-    queue.popleft()
-    queue.append(queue.popleft())
-
-print(queue[0])
+print(find_card(N))
