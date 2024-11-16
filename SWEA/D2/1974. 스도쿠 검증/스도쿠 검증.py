68cd6b5bf5d4a8876a6f211a43797628f5a95e8c
MinGyu <114341409+Minn97u@users.noreply.github.com>
2024-11-17 00:49:08 +0900

    [D2] Title: 스도쿠 검증, Time: 150 ms, Memory: 45,036 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,34 +1,31 @@
 T = int(input())
 
-def sudoku(T):
-    for ty in range(1, T+1):
-        num = [list(map(int, input().split())) for _ in range(9)]
-        result = 1
+for tc in range(1,T+1):
+    lst = list(list(map(int, input().split())) for _ in range(9))
+    result = 1
 
-        for i in range(9):
-            row_check = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            col_check = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for j in range(9):
-                if num[i][j] in row_check:
-                    row_check.remove(num[i][j])
-                else:
-                    result = 0
+    for i in range(9):
+        low_number = [1,2,3,4,5,6,7,8,9]
+        col_number = [1,2,3,4,5,6,7,8,9]
+        for j in range(9):
+            if lst[i][j] in low_number:
+                low_number.remove(lst[i][j])
+            else:
+                result = 0
 
-                if num[j][i] in col_check:
-                    col_check.remove(num[j][i])
-                else:
-                    result = 0
+        for j in range(9):
+            if lst[j][i] in col_number:
+                col_number.remove(lst[j][i])
+            else:
+                result = 0
 
-        for row in range(0,9,3):
-            for col in range(0,9,3):
-                square_check = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                for i in range(3):
-                    for j in range(3):
-                        if num[row + i][col + j] in square_check:
-                            square_check.remove(num[row + i][col + j])
-                        else:
-                            result = 0  # 중복되거나 잘못된 값이 있는 경우
-
-        print("#{} {}".format(ty,result))
-
-sudoku(T)
+    for j in range(0,9,3):
+        for k in range(0,9,3):
+            sqr_number = [1,2,3,4,5,6,7,8,9]
+            for h in range(j,j+3):
+                for i in range(k,k+3):
+                    if lst[h][i] in sqr_number:
+                        sqr_number.remove(lst[h][i])
+                    else:
+                        result = 0
+    print(f"#{tc}",result)
